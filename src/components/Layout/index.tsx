45e7e094929f4ce1107d6a996c372ea4e37fb27a
AdhamNour <adhamnourelwaffaa@gmail.com>
2022-04-02 17:56:22 +0200

    finished the site layout

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,12 +1,26 @@
-import { FunctionComponent } from 'react';
-import SideBar from '../Sidebar';
-import'./index.scss';
-interface LayoutProps {
-    
-}
- 
+import { FunctionComponent } from "react";
+import { Outlet } from "react-router-dom";
+import SideBar from "../Sidebar";
+import "./index.scss";
+interface LayoutProps {}
+
 const Layout: FunctionComponent<LayoutProps> = () => {
-    return ( <SideBar/> );
-}
- 
+  return (
+    <div className="App">
+      <SideBar />
+      <div className="page">
+        <span className="tags top-tags">
+        <span className="bottom-tag-html">&lt;html&gt;</span><br></br>
+
+            &lt;body&gt;</span>
+        <Outlet />
+        <span className="tags bottom-tags">&lt;/body&gt;<br/>
+        <span className="bottom-tag-html">&lt;/html&gt;</span>
+        
+        </span>
+      </div>
+    </div>
+  );
+};
+
 export default Layout;
