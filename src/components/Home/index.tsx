be3517968fe3d18a493e93329a6895c27d0bf4e9
AdhamNour <adhamnourelwaffaa@gmail.com>
2022-04-02 20:51:28 +0200

    finished the site layout

---
+++ src/components/Home/index.tsx
@@ -0,0 +1,11 @@
+import { FunctionComponent } from 'react';
+import './index.scss';
+interface HomeProps {
+    
+}
+ 
+const Home: FunctionComponent<HomeProps> = (props) => {
+    return ( <>Hello</> );
+}
+ 
+export default Home;
