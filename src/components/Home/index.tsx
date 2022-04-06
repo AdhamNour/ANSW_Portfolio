cbf7abdf0ebec89939bf69e276776da07c7244c3
AdhamNour <adhamnourelwaffaa@gmail.com>
2022-04-06 23:35:52 +0200

    finished to 32

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,11 +1,21 @@
-import { FunctionComponent } from 'react';
-import './index.scss';
-interface HomeProps {
-    
-}
- 
+import { FunctionComponent } from "react";
+import "./index.scss";
+interface HomeProps {}
+
 const Home: FunctionComponent<HomeProps> = (props) => {
-    return ( <>Hello</> );
-}
- 
+  return (
+    <div className="container home-page">
+      <div className="text-zone" >
+        <h1>
+          Hello <br /> I am <span>A</span>dham <span>N</span>our
+          <br />
+          Web Developer
+        </h1>
+        <h2>Frontend Developer / Backend Developer / Mobile Developer </h2>
+
+      </div>
+    </div>
+  );
+};
+
 export default Home;
