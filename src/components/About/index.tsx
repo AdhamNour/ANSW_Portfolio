b2514d31bd3e7fb40f6dee30249c87305630c8e9
AdhamNour <adhamnourelwaffaa@gmail.com>
2022-04-25 00:15:03 +0200

    loaders added

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -1,5 +1,14 @@
-import { FunctionComponent, useEffect, useState } from "react";
-import { FaAndroid, FaCss3, FaGitAlt, FaHtml5, FaJsSquare, FaNodeJs, FaReact } from "react-icons/fa";
+import { Fragment, FunctionComponent, useEffect, useState } from "react";
+import {
+  FaAndroid,
+  FaCss3,
+  FaGitAlt,
+  FaHtml5,
+  FaJsSquare,
+  FaNodeJs,
+  FaReact,
+} from "react-icons/fa";
+import Loader from "react-loaders";
 import AnimatedLetters from "../AnimatedLetters";
 import "./index.scss";
 
@@ -14,53 +23,56 @@ const About: FunctionComponent<AboutProps> = () => {
     }, 4000);
   }, []);
   return (
-    <div className="container about-page">
-      <div className="text-zone">
-        <h1>
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={[..."About Me"]}
-            idx={15}
-          />
-        </h1>
-        <p>
-          I’m very ambitious front-end developer looking for a role in
-          established IT company with the opportunity to work with the latest
-          technologies on challenging and diverse projects.
-        </p>
-        <p>
-          I'm quietly confident, naturally curious, and perpetually working on
-          improving my chops one design problem at a time.
-        </p>
-        <p>
-          If I need to define myself in one sentence that would be a family
-          person, father of a beautiful daughter, a sports fanatic, photography
-          enthusiast, and tech-obsessed!!!
-        </p>
-      </div>
-      <div className="stage-cube-cont">
-        <div className="cubespinner">
-          <div className="face1">
-            <FaGitAlt color="" />
-          </div>
-          <div className="face2">
-            <FaHtml5 color="#F06529" />
-          </div>
-          <div className="face3">
-            <FaNodeJs color="#68A063" />
-          </div>
-          <div className="face4">
-            <FaReact color="#5ED4F4" />
-          </div>
-          <div className="face5">
-            <FaJsSquare color="#EFD81D" />
-          </div>
-          <div className="face6">
-            <FaAndroid color="3DDC84" />
+    <Fragment>
+      <div className="container about-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={[..."About Me"]}
+              idx={15}
+            />
+          </h1>
+          <p>
+            I’m very ambitious front-end developer looking for a role in
+            established IT company with the opportunity to work with the latest
+            technologies on challenging and diverse projects.
+          </p>
+          <p>
+            I'm quietly confident, naturally curious, and perpetually working on
+            improving my chops one design problem at a time.
+          </p>
+          <p>
+            If I need to define myself in one sentence that would be a family
+            person, father of a beautiful daughter, a sports fanatic,
+            photography enthusiast, and tech-obsessed!!!
+          </p>
+        </div>
+        <div className="stage-cube-cont">
+          <div className="cubespinner">
+            <div className="face1">
+              <FaGitAlt color="" />
+            </div>
+            <div className="face2">
+              <FaHtml5 color="#F06529" />
+            </div>
+            <div className="face3">
+              <FaNodeJs color="#68A063" />
+            </div>
+            <div className="face4">
+              <FaReact color="#5ED4F4" />
+            </div>
+            <div className="face5">
+              <FaJsSquare color="#EFD81D" />
+            </div>
+            <div className="face6">
+              <FaAndroid color="3DDC84" />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      <Loader type="pacman" active={true} />
+    </Fragment>
   );
 };
 
--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,4 +1,5 @@
-import { FunctionComponent, useEffect, useState } from "react";
+import { Fragment, FunctionComponent, useEffect, useState } from "react";
+import Loader from "react-loaders";
 import { Link } from "react-router-dom";
 import AnimatedLetters from "../AnimatedLetters";
 import "./index.scss";
@@ -18,45 +19,48 @@ const Home: FunctionComponent<HomeProps> = (props) => {
   }, []);
 
   return (
-    <div className="container home-page">
-      <div className="text-zone">
-        <h1>
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={helloArray}
-            idx={15 + firstNameArray.length - helloArray.length}
-          />{" "}
-          <br />{" "}
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={[..."I'm"]}
-            idx={15 + firstNameArray.length}
-          ></AnimatedLetters>{" "}
-          <span className="first-latter">A</span>
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={firstNameArray}
-            idx={15 + firstNameArray.length}
-          />
-          <span className="first-latter secondary">N</span>
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={SecondNameArray}
-            idx={15 + firstNameArray.length + SecondNameArray.length}
-          />
-          <br />
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={jobArray}
-            idx={15 + firstNameArray.length + SecondNameArray.length}
-          />
-        </h1>
-        <h2>Frontend Developer / Backend Developer / Mobile Developer </h2>
-        <Link to={"/contact"} className="flat-button">
-          CONTACT ME
-        </Link>
+    <Fragment>
+      <div className="container home-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={helloArray}
+              idx={15 + firstNameArray.length - helloArray.length}
+            />{" "}
+            <br />{" "}
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={[..."I'm"]}
+              idx={15 + firstNameArray.length}
+            ></AnimatedLetters>{" "}
+            <span className="first-latter">A</span>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={firstNameArray}
+              idx={15 + firstNameArray.length}
+            />
+            <span className="first-latter secondary">N</span>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={SecondNameArray}
+              idx={15 + firstNameArray.length + SecondNameArray.length}
+            />
+            <br />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={jobArray}
+              idx={15 + firstNameArray.length + SecondNameArray.length}
+            />
+          </h1>
+          <h2>Frontend Developer / Backend Developer / Mobile Developer </h2>
+          <Link to={"/contact"} className="flat-button">
+            CONTACT ME
+          </Link>
+        </div>
       </div>
-    </div>
+      <Loader type="pacman" active={true} />
+    </Fragment>
   );
 };
 
